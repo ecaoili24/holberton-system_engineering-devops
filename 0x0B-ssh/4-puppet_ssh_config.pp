d283b7a8e3a4d07de5e0fc5bf754c2923f6524e2
ecaoili24 <erika.caoili@gmail.com>
2020-03-29 20:40:33 +0000

    fixed typo

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Sets up your client SSH configuration file to use a private key
 include stdlib
 
-file_line { 'USE privat key':
+file_line { 'USE private key':
   path  =>  '/etc/ssh/ssh_config',
   line  =>  'IdentityFile ~/.ssh/holberton',
 }

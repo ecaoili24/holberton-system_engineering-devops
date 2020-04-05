1f3682ba0b8e418374c3dc2c41cbc1e4246c8e41
ecaoili24 <erika.caoili@gmail.com>
2020-04-05 02:49:41 +0000

    Fixed puppet lint issues

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,12 @@
 # Custom HTTP header response, so that it configures a brand new Ubuntu
 # machine to the requirements asked in this task
 exec { 'http':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && var="add_header X-Served-By: $HOSTNAME" && sudo sed -i "50i$var" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get update &&
+     	      sudo apt-get -y install nginx && var="add_header X-Served-By:
+	      $HOSTNAME" &&
+     	      sudo sed -i "50i$var" /etc/nginx/sites-available/default &&
+     	      sudo service nginx start',
   provider => shell
-}
+  }
+
+

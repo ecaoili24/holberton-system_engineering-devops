6e55abac324ab0b4224d0bbb4ec53ec1722dec73
ecaoili24 <erika.caoili@gmail.com>
2020-05-07 18:34:56 +0000

    Initial commit of task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the total amount of subscribers
+for a given subreddit. If an invalid subreddit is given, the func
+should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the num of subscribers """
+    r = requests.get("https://reddit.com/r/{}/about.json".format(subreddit),
+                     headers={"User-Agent": "custom"})
+
+    if(r.status_code == 200):
+        return r.json().get("data").get("subscribers")
+    return 0

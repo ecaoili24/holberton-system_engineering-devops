93436fb9a1b426b4cca4a8b6802258951f008b99
ecaoili24 <erika.caoili@gmail.com>
2020-05-29 05:24:33 +0000

    Wrote the the correct exec process name

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Fixes the # of failed requests to 0
 
-exec { 'nginx-fix':
+exec { 'fix--for-nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g'
   	      /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
